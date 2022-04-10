e78cf57670b922f9774fe0b0b84037f11fcaa3d7
Raúl Fernández Cobo <rfc00021@red.ujaen.es>
2022-04-10 13:05:10 +0200

    mas cambios

--- games/media/games/tutorial/tutorial.game.en.js
+++ games/media/games/tutorial/tutorial.game.en.js
@@ -33,10 +33,12 @@ undum.game.situations = {
         <p>Tras día y medio de travesía, el templo de Ver-duleria se deja ver al fin. Este se encuetra en lo alto de una colina\
         rodeado por un bosque de escasos árboles. Distingues un pequeño sendero que parece dirigirse al templo y decides tomarlo. Se trata de un\
         pequeño edificio de dos plantas hecho de piedra marrón desgastada. En la entrada te recibe un monje vestido con un hábito marron y blanco.\
-        - Bienvenido joven viajero, a que se debe su visita?\
-        <a href = 'explicar'>Explicar tu situación</a>\
-        <a href ='noexplicar'>Desconfiar del monje y no explicar tu situación</a>\
-         </p>"
+        - Bienvenido joven viajero, a que se debe su visita?</p>\
+        <ul class='options'>\
+        <li><a href = 'explicar'>Explicar tu situación</a></li>\
+        <li><a href ='noexplicar'>Desconfiar del monje y no explicar tu situación</a></li>\
+        </ul>\
+         <br>"
     ),
     //Situacion en la que explicas al monje
     explicar: new undum.SimpleSituation(
@@ -44,17 +46,35 @@ undum.game.situations = {
         <img src='media/games/tutorial/woodcut1.png' class='float_right'>\
         <p>Tras explicar la situacción con gusto el monje accede a llevarte hacia \
         donde se encuentra la Remolacha, en un sendero que rodea el templo. A mitad de camino\
-        el monje se detiene y parece no saber el camino a tomar, desconcertado le preguntas: </p>\
+        el monje se detiene y parece no saber el camino a tomar, desconcertado le preguntas:\
         - ¿Sabe usted por donde ir?\
         - Sí, disculpa, mi memoria ya no es lo que era, es por aquí\
         El camino es algo laberíntico, y trás un rato detrás del monje empiezas a sospechar\
         que estáis caminando en círculos. De pronto el monje se agacha y arranca una hierba.\
         - Al fin, aquí está la preciada Remolacha\
         El monje te entrega la planta.\
-        <a href ='examinar'>Examinar planta(Hacer tirada)</a>\
+        <a href ='./examinar'>Examinar planta(Hacer tirada)</a>\
         (Si tienes la sabiduria necesaria )Al examinar la planta te das cuenta de que es una planta común y la has visto varias veces\
         en el camino, el monje te está engañando, ¡quizás sea un impostor!\
-        No te queda otra que <a href= 'combatemonje'>atacarle</a> </p>"
+        No te queda otra que <a href = 'combatemonje'> atacarle </a> </p>",
+        {
+            actions: 
+            {
+                "examinar":function(character, system, action){
+                    var dado = jsRandom.get(1,10);
+									if ((dado+character.qualities.sabiduria) > 5){
+										system.setQuality("tirada", dado);
+										system.write($("xxx").html());
+									}else{
+										system.setQuality("stamina", dado);
+										system.write("xxx");
+									}
+                }
+            }
+
+
+
+        }
     ),
     //si no tienes la sabiduria necesaria
     nosabiduria: new undum.SimpleSituation(
@@ -62,9 +82,6 @@ undum.game.situations = {
        <p>Debe de ser la Remolacha, piensas. Satisfecho con tu labor, decides volver al castillo para entregarle la hierba al Duque y obtener tu recompensa.\
        Una vez allí eres muy bien recibido, el Duque Agnar te da las gracias y entrega la planta a sus expertos. Estos le comunican enseguida que la planta\
        que has traído no es la Remolacha sino una planta muy común. El enfado del Duque es tal, que ordena tu decapitación pública inmediatamente... FIN DEL JUEGO  \ </p>"
-
-
-
 
     ),
         //Situación en la que no le explicas al monje y decides pasar
@@ -74,122 +91,55 @@ undum.game.situations = {
         <p>Decides entrar en el templo sin el consentimiento del monje, \
         no te fías ni un pelo de él. Este se ve reacio a dejaros entrar y de pronto\
         saca una larga y afilada espada. Al moverse bruscamente, un colgante asoma de su hábito. LLeva \
-        la marca de la flor Lis. Te enzarzas en un duro <a href = 'combate'>combate</a> con él, resulta ser un asesino bien entrenado"
+        la marca de la flor Lis. La misma que llevaba Felipo. Te enzarzas en un duro <a href='combatemonje'>combate</a> con él, resulta ser un asesino bien entrenado</p>"
     ),
 
-    //Combate con el monje
+    //Combate con el monje(falta escribirlo)
     combatemonje: new undum.SimpleSituation(
         "<h1>COMBATE</h1>\
         <img src='media/games/tutorial/woodcut1.png' class='float_right'>\
-        <p>Combate con el monje</p>"
+        <p>Combate con el monje\
+        <a href='aftercombate'>Siguiente pagina</a></p>"
     ),
 
-    aftercombate: new undum.Situation(
+    aftercombate: new undum.SimpleSituation(
         "<h1>EL TEMPLO</h1>\
-        <img src='media/games/tutorial/woodcut1.png' class='float_right'>\
         <p>Tras derrotar al asesino, observas una columna de humo proveniente del interior del templo. Se trata de unos hombres que están\
         quemando toda la información relacionada con la planta. Te apresuras pero para cuando llegas los hombres han huido. La sala resulta ser una biblioteca antigua\
         con una gran fogata en el centro donde están quemándose <a href= './libros'>los libros</a>. Observas una <a href ='./ventana'>ventana</a> en la parte derecha de la sala. </p>\
         ",
-
         {
-            actions: {
+            actions: 
+            {
                 'libros': "<p> De entre las llamas rescatas un libro con información de la hierba. Está en mal estado pero consigues rescatar un pequeño mapa que dice llevar a un lugar\
                 donde es posible encontrar la planta. <a href = 'mapa'>Decides seguir el mapa </p>",
                 'ventana':"<p> Te asomas a la ventana y ves a los hombres que han quemado los libros huir a toda velocidad. No merece la pena perseguirlos</p>" 
             }
-    }),
+        }
+    ),
    
     ataquefinal: new undum.SimpleSituation(
         "<p>Al salir de la caverna te encuentras con GAL, el asesino que contrató Felipo para impedirte lograr tu misión. En su rostro contemplas el miedo, como de haber observado tu enfrentamiento con la bestia. </p>\
-        De pronto se da la vuelta y ante tu sorpresa se quita de su sucio dedo un anillo de tres rubíes.\
-        <p>If you've been watching carefully, you will have noticed that\
-        parts of the text have been disappearing when you move between\
-        situations. This isn't a bug! One of the aims of Undum is to give\
-        game designers the ability to make the transcript of\
-        the game read as a coherent piece of narrative. However, you often\
-        need chunks of text that do nothing but offer the reader choices.\
-        Undum defines a special CSS class to add to your HTML for this\
-        (remember generated content is just HTML). It is <em>transient</em>,\
-        and can be applied to paragraphs, <em>div</em>s, or just\
-        <em>span</em>s<span class='transient'> (such as this one)</span>.</p>\
-        \
-        <p>You may also have noticed that, when you move situations, all the\
-        links in the previous situation turn into regular text. This is to\
-        stop you backtracking and trying previous options when you've already\
-        committed to one. In other H-IF systems, this is\
-        done by completely removing the content from previous pages.\
-        That prevents you going back and reading your story, however.</p>\
-        \
-        <p class='transient'>The 'Different Kinds of Links' topic has more\
-        about these links.\
-        Let's return to the <a href='hub'>topic list</a>.</p>",
-        {
-            heading: "Disappearing Content",
-            diplayOrder: 2,
-            tags: ["topic"]
-        }
+        De pronto se da la vuelta y ante tu sorpresa se quita de su sucio dedo un anillo de tres rubíes. Es el mismo anillo que lleva Felipo. Es imprescindible conseguirlo, pues es una prueba contra él. \
+        GAL lanza el anillo a un lodazal para que se hunda en él y dice : \
+        - Jamás conseguiras el anillo\
+        <p>Acto seguido se lanza al ataque. Es tan veloz que no tienes tiempo de reaccionar y y consigue hacerte un corte en el brazo. Desenvainas tu espada mientras observas como el anillo, la única prueba contra\
+         Felipo, se hunde en el lodo. El combate es trepidante, es sin duda el mejor de los asesinos contratados por Felipo, y tu te encuentras exhausto tras el combate con la bestia. Decides desequilibrarlo \
+        y zambullarte en el lodazal para tratar de coger el anillo pero justo cuando un palmo separa tu dedo de este GAL agarra tu pierna y tira con fuerza. El lodo es denso y espeso y aprovechas que tu enemigo  \
+        ha perdido su arma en él para darle una última oportunidad de vivir, con tu espada rozando su cuello. GAL desvía su mirada de la tuya para observar el anillo. Tu te giras para contemplarlo también y en ese despiste\
+        aprovecha para coger su espada y blandirla contra ti. Consigues esquivarla y esta vez no concedes ninguna oportunidad. Le das muerte a tu enemigo. Rápidamente sumerges bien tu brazo en el lodo para sacar el anillo.</p>\
+        <br>\
+		<p class='transient'><a href='vueltacastillo'>Siguiente Página</a></p>",
     ),
-    sticky: new undum.SimpleSituation(
-        "<p>There are three types of link in Undum. The first two we've seen\
-        in previous topics:\
-        links to change situation and links to carry out an action. When you\
-        include a link in your output, Undum parses it and wires it up\
-        correctly. If you create a link with a HTML <em>href</em> attribute\
-        containing just a name ('ballroom', for\
-        example) this will send the character to the situation with that\
-        name. Links\
-        with two components ('ballroom/view-painting', for example) send\
-        the character to a new situation <em>and then</em> carry out the\
-        named action ('view-painting' in this case). To carry out an action\
-        in the current situation, you can replace the situation name with a\
-        dot (so it would be './view-painting'). In all cases, if the\
-        character is already in that situation, then the situation's\
-        <em>enter</em> method won't be called again.</p>\
-        \
-        <img src='media/games/tutorial/woodcut2.png' class='float_left'>\
-        <p>The third type of link, then, is a general hyperlink. If your\
-        link doesn't consist of a single element or pair of elements, as\
-        above, then Undum will guess that you have a normal hyperlink. As\
-        <a href='http://news.bbc.co.uk' class='sticky'>in this link</a>.\
-        If you have a link that <em>does</em> look like an Undum link, you\
-        can still force Undum not to interpret it as an action or situation\
-        move, by adding the CSS class <em>raw</em> to the HTML <em>a</em> tag.\
-        links that don't have the <em>raw</em> class, but that are considered\
-        to be non-Undum links (like the link above), will have <em>raw</em>\
-        added to them before display. This could allow you to style external\
-        links differently, as we have done here.</p>\
-        \
-        <p>In the last situation I said you can prevent links from being\
-        turned into regular text when you move situations. This is done\
-        by another CSS class: <em>sticky</em>. When you\
-        <a href='oneshot'>leave this situation</a>, you'll notice the\
-        external link stays active. This can allow you to have options that\
-        stay valid throughout the narrative, for example, such as a spell to\
-        teleport home.</p>",
-        {
-            tags: ["topic"],
-            displayOrder: 3,
-            heading: "Different Kinds of Links"
-        }
+    vueltacastillo: new undum.SimpleSituation(
+        "<p>Regresas al castillo por fin para entregarle la hierba al Duque y obtener tu recompensa. Una vez allí eres muy bien recibido. En la sala del trono, un perplejo Felipo observa como entras con elegancia.\
+        <a href= 'noperdonar'>Decides mostrar el anillo para acusar a Felipo de intentar matarte</a>\
+        <a href='perdonar'>Decides perdonarlo y guardar el anillo para venderlo por una gran cantidad de dinero</a>"
     ),
-    oneshot: new undum.SimpleSituation(
-        "<p>There is one final option for links. If you give a link\
-        the <em>once</em> CSS class, then that link will disappear\
-        after it is clicked. This is  used (as in\
-        <a href='./one-time-action' class='once'>this link</a>) for\
-        actions that you only want to be possible once. There is no\
-        point using 'once' on situation links because they'll be turned\
-        into text as soon as you click them anyway (unless they are also\
-        <em>sticky</em>, of course).</p><p>Once links are useful\
-        for actions such as examining an object more carefully. You\
-        don't want lots of repeated descriptions, so making the link\
-        a <em>once</em> link is more user friendly.</p>\
-        <p>If you have more than one link to the same action, then all\
-        matching links will be removed, so you don't have to worry about\
-        the player having an alternative way to carry out the action.</p>\
-        <p class='transient'>After you've clicked the link, let's\
-        <a href='hub'>move on</a>.</p>",
+    noperdonar: new undum.SimpleSituation(
+        "<p>Al mostrar el anillo Felipo coge a su hermano y le pone una daga en el cuello para intentar escapar con su guardia personal. \
+        Sin embargo sus deseos se ven frustrados cuando de una patada de su esposa en los miembros inferiores este cae al suelo y comienza a llorar. Mas tarde es encerrado por traidor.\
+        El Duque Agnar te da las gracias por tu labor y tu merecida recompensa. FIN  </p>",
         {
             actions: {
                 "one-time-action": "<p>As I said, one time actions are\
@@ -433,13 +383,45 @@ undum.game.situations = {
 
 // ---------------------------------------------------------------------------
 /* The Id of the starting situation. */
-undum.game.start = "start";
+undum.game.start = "templo";
 
 // ---------------------------------------------------------------------------
 /* Here we define all the qualities that our characters could
  * possess. We don't have to be exhaustive, but if we miss one out then
  * that quality will never show up in the character bar in the UI. */
 undum.game.qualities = {
+	vida: new undum.NumericQuality(
+		"Vida", {priority:"0001", group:'stats'}
+	),
+	
+	fuerza: new undum.NumericQuality(
+		"Fuerza", {priority:"0001", group:'stats'}
+	),
+	
+	agilidad: new undum.NumericQuality(
+		"Agilidad", {priority:"0001", group:'stats'}
+	),
+	
+	defensa: new undum.NumericQuality(
+		"defensa", {priority:"0001", group:'stats'}
+	),
+	
+	sabiduria: new undum.NumericQuality(
+		"Sabiduía", {priority:"0001", group:'stats'}
+	),
+	
+	sigilo: new undum.NumericQuality(
+		"Sigilo", {priority:"0001", group:'stats'}
+	),
+	
+	cuchillo: new undum.NumericQuality(
+        "Cuchillo", {priority:"0003", group:'objetos', onDisplay:"&#10003;"}
+    ),
+	
+	tirada: new undum.NumericQuality(
+        "Tirada", {priority:"0002", group:'dado', onDisplay:"&#10003;"}
+    )
+	/*
     skill: new undum.IntegerQuality(
         "Skill", {priority:"0001", group:'stats'}
     ),
@@ -450,13 +432,13 @@ undum.game.qualities = {
         "<span title='Skill, Stamina and Luck are reverently borrowed from the Fighting Fantasy series of gamebooks. The words representing Luck are from the FUDGE RPG. This tooltip is illustrating that you can use any HTML in the label for a quality (in this case a span containing a title attribute).'>Luck</span>",
         {priority:"0003", group:'stats'}
     ),
-
     inspiration: new undum.NonZeroIntegerQuality(
         "Inspiration", {priority:"0001", group:'progress'}
     ),
+	
     novice: new undum.OnOffQuality(
         "Novice", {priority:"0002", group:'progress', onDisplay:"&#10003;"}
-    )
+    )*/
 };
 
 // ---------------------------------------------------------------------------
@@ -467,17 +449,20 @@ undum.game.qualities = {
  * non-existent group. */
 undum.game.qualityGroups = {
     stats: new undum.QualityGroup(null, {priority:"0001"}),
-    progress: new undum.QualityGroup('Progress', {priority:"0002"})
+    dado: new undum.QualityGroup('Dado', {priority:"0002"}),
+	objetos: new undum.QualityGroup('Objetos' , {priority:"0003"})
 };
 
 // ---------------------------------------------------------------------------
 /* This function gets run before the game begins. It is normally used
  * to configure the character at the start of play. */
 undum.game.init = function(character, system) {
-    character.qualities.skill = 12;
-    character.qualities.stamina = 12;
-    character.qualities.luck = 0;
-    character.qualities.novice = 1;
-    character.qualities.inspiration = 0;
-    system.setCharacterText("<p>You are starting on an exciting journey.</p>");
+	character.qualities.vida = 10;
+    character.qualities.fuerza = 3;
+	character.qualities.agilidad = 3;
+	character.qualities.defensa = 3;
+	character.qualities.sabiduria = 3;
+	character.qualities.sigilo = 3;
+	character.qualities.cuchillo = 0;
+	character.qualities.tirada = 0;
 };
