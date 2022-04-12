14c97bb0204dfd459463e20c66dd5c8a2db73d5d
Raúl Fernández Cobo <rfc00021@red.ujaen.es>
2022-04-12 18:53:05 +0200

    FInalización historia en undum + progreso

--- games/media/games/tutorial/tutorial.game.en.js
+++ games/media/games/tutorial/tutorial.game.en.js
@@ -32,8 +32,8 @@ undum.game.situations = {
         "<h1>EL TEMPLO</h1>\
         <p>Tras día y medio de travesía, el templo de Ver-duleria se deja ver al fin. Este se encuetra en lo alto de una colina\
         rodeado por un bosque de escasos árboles. Distingues un pequeño sendero que parece dirigirse al templo y decides tomarlo. Se trata de un\
-        pequeño edificio de dos plantas hecho de piedra marrón desgastada. En la entrada te recibe un monje vestido con un hábito marron y blanco.\
-        - Bienvenido joven viajero, a que se debe su visita?</p>\
+        pequeño edificio de dos plantas hecho de piedra marrón desgastada. En la entrada te recibe un monje vestido con un hábito marron y blanco.</p>\
+        <p class = 'dialogo'>― Bienvenido joven viajero, a que se debe su visita?</p>\
         <ul class='options'>\
         <li><a href = 'explicar'>Explicar tu situación</a></li>\
         <li><a href ='noexplicar'>Desconfiar del monje y no explicar tu situación</a></li>\
@@ -47,34 +47,31 @@ undum.game.situations = {
     ),
     //Situacion en la que explicas al monje
     explicar: new undum.SimpleSituation(
-        "<h1>EL TEMPLO</h1>\
-        <img src='media/games/tutorial/woodcut1.png' class='float_right'>\
+        "<h1>SENDERO</h1>\
         <p>Tras explicar la situacción con gusto el monje accede a llevarte hacia \
         donde se encuentra la Remolacha, en un sendero que rodea el templo. A mitad de camino\
-        el monje se detiene y parece no saber el camino a tomar, desconcertado le preguntas:\
-        - ¿Sabe usted por donde ir?\
-        - Sí, disculpa, mi memoria ya no es lo que era, es por aquí\
-        El camino es algo laberíntico, y trás un rato detrás del monje empiezas a sospechar\
-        que estáis caminando en círculos. De pronto el monje se agacha y arranca una hierba.\
-        - Al fin, aquí está la preciada Remolacha\
-        El monje te entrega la planta.\
-        <a href ='./examinar'>Examinar planta(Hacer tirada)</a>\
-        (Si tienes la sabiduria necesaria )Al examinar la planta te das cuenta de que es una planta común y la has visto varias veces\
-        en el camino, el monje te está engañando, ¡quizás sea un impostor!\
-        No te queda otra que <a href = 'combatemonje'> atacarle </a> </p>",
+        el monje se detiene y parece no saber el camino a tomar, desconcertado le preguntas:</p>\
+        <p class = 'dialogo'>― ¿Sabe usted por donde ir?</p>\
+        <p class = 'dialogo'>― Sí, disculpa, mi memoria ya no es lo que era, es por aquí. ―Responde el monje</p>\
+        <p>El camino es algo laberíntico, y trás un rato detrás del monje empiezas a sospechar\
+        que estáis caminando en círculos. De pronto el monje se agacha y arranca una hierba.</p>\
+        <p class = 'dialogo'>― Al fin, aquí está la planta que buscas!</p>\
+       <p> El monje te entrega la planta.\
+        <a href ='./examinar' class='once'>Examinar planta(Tirada de percepción)</a></p>",
         {
             actions: 
             {
                 'examinar':function(character, system, action){
-                    system.write("<p>xxx</p>");
                     var dado = jsRandom.get(1,10);
                     
-									if ((dado+character.qualities.sabiduria) > 0){
+									if ((dado+character.qualities.sabiduria) > 5){
 										system.setQuality("tirada", dado);
-										system.write("<p>xxx</p>");
+										system.write("<p>Al examinar la planta te das cuenta de que es una planta común y la has visto varias veces\
+                                            en el camino, el monje te está engañando, ¡quizás sea un impostor!\
+                                            No te queda otra que <a href = 'combatemonje'> atacarle </a> </p>");
 									}else{
 										system.setQuality("stamina", dado);
-										system.write("<p>xxx</p>");
+										system.write("<p><a href='nosabiduria'>Continuar</a></p>");
 									}
                 }
             }
@@ -88,7 +85,7 @@ undum.game.situations = {
        " <h1>REGRESO AL CASTILLO</h1>\
        <p>Debe de ser la Remolacha, piensas. Satisfecho con tu labor, decides volver al castillo para entregarle la hierba al Duque y obtener tu recompensa.\
        Una vez allí eres muy bien recibido, el Duque Agnar te da las gracias y entrega la planta a sus expertos. Estos le comunican enseguida que la planta\
-       que has traído no es la Remolacha sino una planta muy común. El enfado del Duque es tal, que ordena tu decapitación pública inmediatamente... FIN DEL JUEGO  \ </p>"
+       que has traído no es la Remolacha sino una planta muy común. El enfado del Duque es tal, que ordena tu decapitación pública inmediatamente... </p><p><b>FIN DEL JUEGO</b></p>"
 
     ),
         //Situación en la que no le explicas al monje y decides pasar
@@ -98,30 +95,24 @@ undum.game.situations = {
         <p>Decides entrar en el templo sin el consentimiento del monje, \
         no te fías ni un pelo de él. Este se ve reacio a dejaros entrar y de pronto\
         saca una larga y afilada espada. Al moverse bruscamente, un colgante asoma de su hábito. LLeva \
-        la marca de la flor Lis. La misma que llevaba Felipo. Te enzarzas en un duro <a href='combatemonje'>combate</a> con él, resulta ser un asesino bien entrenado</p>"
+        la marca de la flor Lis. La misma que llevaba Felipo. Te enzarzas en un duro <a href='combatemonje'>combate</a> con él.</p>"
     ),
 
     //Combate con el monje(falta escribirlo)
     combatemonje: new undum.SimpleSituation(
-        "<h1>COMBATE</h1>\
-        <img src='media/games/tutorial/woodcut1.png' class='float_right'>\
-        <p>(pelea_monje)El monje se deshace de su vestimenta, resulta ser un asesino contratado por Felipo para evitar que consigas la planta. Es musculoso y maloliento asesino desenvaina su larga espada y espera tu ataque.\
-         (vives) Le arrojas tu cuchillo al brazo que sujeta la espada consiguiendo que este la suelte. Corres a cogerla y consigues esquivar un puñetazo. Ya tienes su espada, él se arranca el \
-         cuchillo que le has lanzado del brazo y te lo lanza rápidamente. Recibes un corte en el brazo pero cuando menos se lo espera su vientre es atravesado por su propia espada empuñada por ti.\
-         (mueres) Le arrojas tu cuchillo al brazo que sujeta la espada consiguiendo que este la suelte. Corres a cogerla pero el asesino te derriba de un puñetazo. Acto seguido recoge su espada y la clava en tu pecho. \
-        <a href='aftercombate'>Siguiente pagina</a></p>",
+        "",
         {
 			enter:function(character, system, action) {
 				system.write($("#pelea_monje").html());
                 var dado = jsRandom.get(1,10);
-					if(dado + character.qualities.cuchillo > 5){
-						system.write($("#vives").html());
+					if(dado + character.qualities.cuchillo > 3){
+						system.write($("#vives_monje").html());
                         system.write("<p><a href='aftercombate'>Siguiente pagina</a></p>");
 						system.setQuality("cuchillo", character.qualities.cuchillo-1);
 						system.setQuality("vida", character.qualities.vida-3);
 						
 					}else{
-						system.write($("#mueres").html());
+						system.write($("#mueres_monje").html());
 					}
 				}
 		}
@@ -131,7 +122,7 @@ undum.game.situations = {
         "<h1>EL TEMPLO</h1>\
         <p>Tras derrotar al asesino, observas una columna de humo proveniente del interior del templo. Se trata de unos hombres que están\
         quemando toda la información relacionada con la planta. Te apresuras pero para cuando llegas los hombres han huido. La sala resulta ser una biblioteca antigua\
-        con una gran fogata en el centro donde están quemándose <a href= './libros'>los libros</a>. Observas una <a href ='./ventana'>ventana</a> en la parte derecha de la sala. </p>\
+        con una gran fogata en el centro donde están quemándose <a href= './libros' class  ='once'>los libros</a>. Observas una <a href ='./ventana' class  ='once'>ventana</a> en la parte derecha de la sala. </p>\
         ",
         {
             actions: 
@@ -144,10 +135,10 @@ undum.game.situations = {
     ),
    
     ataquefinal: new undum.SimpleSituation(
-        "<p>Al salir de la caverna te encuentras con GAL, el asesino que contrató Felipo para impedirte lograr tu misión. En su rostro contemplas el miedo, como de haber observado tu enfrentamiento con la bestia. </p>\
-        De pronto se da la vuelta y ante tu sorpresa se quita de su sucio dedo un anillo de tres rubíes. Es el mismo anillo que lleva Felipo. Es imprescindible conseguirlo, pues es una prueba contra él. \
+        "<h1> ATAQUE FINAL </h1><p>Al salir de la caverna te encuentras con GAL, el asesino que contrató Felipo para impedirte lograr tu misión. En su rostro contemplas el miedo, como de haber observado tu enfrentamiento con la bestia. </p>\
+        De pronto se da la vuelta y ante tu sorpresa se quita de su sucio dedo un anillo de tres rubíes. Recuerdas que es el mismo anillo que llevaba Felipo el día que lo viste en el castillo. Es imprescindible conseguirlo, pues es una prueba contra él. \
         GAL lanza el anillo a un lodazal para que se hunda en él y dice : \
-        - Jamás conseguiras el anillo\
+        <p class= 'dialogo'>― Podrás vencerme a mi, pero jamás acabareis con Felipo. Aunque dudo que pueda contra mi un necio de tu calibre.</p>\
         <p>Acto seguido se lanza al ataque. Es tan veloz que no tienes tiempo de reaccionar y y consigue hacerle un corte en el brazo. Desenvainas tu espada mientras observas como el anillo, la única prueba contra\
          Felipo, se hunde en el lodo. El combate es trepidante, es sin duda el mejor de los asesinos contratados por Felipo, y tu te encuentras exhausto tras el combate con la bestia. Decides desequilibrarlo \
         y zambullarte en el lodazal para tratar de coger el anillo pero justo cuando un palmo separa tu dedo de este GAL agarra tu pierna y tira con fuerza. El lodo es denso y espeso y aprovechas que tu enemigo  \
@@ -157,26 +148,28 @@ undum.game.situations = {
 		<p class='transient'><a href='vueltacastillo'>Siguiente Página</a></p>",
     ),
     vueltacastillo: new undum.SimpleSituation(
-        "<p>Regresas al castillo por fin para entregarle la hierba al Duque y obtener tu recompensa. Una vez allí eres muy bien recibido. En la sala del trono, un perplejo Felipo observa como entras con elegancia.\
-        <a href= 'noperdonar'>Decides mostrar el anillo para acusar a Felipo de intentar matarte</a>\
-        <a href='perdonar'>Decides perdonarlo y guardar el anillo para venderlo por una gran cantidad de dinero</a>"
+        "<h1>REGRESO AL CASTILLO</h1>\
+        <p>Regresas al castillo por fin para entregarle la hierba al Duque y obtener tu recompensa. Una vez allí eres muy bien recibido. En la sala del trono, un perplejo Felipo observa como entras con elegancia.\
+        <ul class='options'>\
+        <li><a href= 'noperdonar'>Decides mostrar el anillo para acusar a Felipo de intentar matarte</a></li>\
+        <li><a href='perdonar'>Decides perdonarlo y guardar el anillo para venderlo por una gran cantidad de dinero</a></li>\
+        </ul>\
+         <br>"
     ),
     noperdonar: new undum.SimpleSituation(
-        "<p>Al mostrar el anillo Felipo coge a su hermano y le pone una daga en el cuello para intentar escapar con su guardia personal. \
-        Sin embargo sus deseos se ven frustrados cuando de una patada de su esposa en los miembros inferiores este cae al suelo y comienza a llorar. Mas tarde es encerrado por traidor.\
+        "<h1>REGRESO AL CASTILLO</h1>\
+        <p>Al mostrar el anillo Felipo coge a su hermano y le pone una daga en el cuello para intentar escapar con su guardia personal. \
+        Sin embargo sus deseos se ven frustrados cuando de una patada de su esposa en los miembros inferiores este cae al suelo y comienza a llorar. Más tarde es encerrado por traidor.\
         El Duque Agnar te da las gracias por tu labor y te entrega tu merecida recompensa.<p><b>FIN</b></p>",
         {
-            actions: {
-                "one-time-action": "<p>As I said, one time actions are\
-                                   mostly used to describe something in\
-                                   more detail, where you don't want the\
-                                   same descriptive text repeated over and\
-                                   over</p>"
-            }
+            enter:function(character, system, action) {
+                system.setQuality("progreso", character.qualities.progreso+100);
+                }
         }
     ),
     perdonar: new undum.SimpleSituation(
-        "<p>El Duque Agnar te da las gracias por tu labor y te entrega una merecida recompensa. Felipo, \
+        "<h1>REGRESO AL CASTILLO</h1>\
+        <p>El Duque Agnar te da las gracias por tu labor y te entrega una merecida recompensa. Felipo, \
         que aún desconfía de ti, ordena que te maten nada mas salir del castillo. Allí, sus guardias te pillan desprevenido\
         y consiguen derrotarte sin mucho esfuerzo.</p> <p><b>FIN DEL JUEGO</b></p>"
     ),
@@ -184,7 +177,7 @@ undum.game.situations = {
 
 // ---------------------------------------------------------------------------
 /* The Id of the starting situation. */
-undum.game.start = "templo";
+undum.game.start = "ataquefinal";
 
 // ---------------------------------------------------------------------------
 /* Here we define all the qualities that our characters could
@@ -223,7 +216,7 @@ undum.game.qualities = {
         "Tirada", {priority:"0002", group:'dado', onDisplay:"&#10003;"}
     ),
     progreso: new undum.NumericQuality(
-        "", {priority:"0004", group:'progreso', onDisplay:"&#10003;"}
+        "%", {priority:"0004", group:'progreso', onDisplay:"&#10003;"}
     )
 	/*
     skill: new undum.IntegerQuality(
